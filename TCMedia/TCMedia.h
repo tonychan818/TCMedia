e03b0d3decfd4533fec5a0d37c94e8c94e5d9265
Tony <tony@Tonys-MacBook-Pro.local>
2015-08-31 19:31:41 +0800

    Initial Commit

---
+++ TCMedia/TCMedia.h
@@ -0,0 +1,19 @@
+//
+//  TCMedia.h
+//  TCMedia
+//
+//  Created by Tony on 31/8/15.
+//  Copyright (c) 2015 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TCMedia.
+FOUNDATION_EXPORT double TCMediaVersionNumber;
+
+//! Project version string for TCMedia.
+FOUNDATION_EXPORT const unsigned char TCMediaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TCMedia/PublicHeader.h>
+
+
